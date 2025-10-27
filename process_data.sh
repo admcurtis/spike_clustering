2d8149f1d666943340ae4b87fcd18844f2bfdfc8
admcurtis <mail@adamjcurtis.com>
2025-10-27 16:28:05 +0000

    process_data.sh now runs on all ppt recursively

--- process_data.sh
+++ process_data.sh
@@ -5,25 +5,31 @@ PATH=$PATH:/home/adam/workspace/ucl/spike_clustering/combinato
 PYTHONPATH=$PYTHONPATH:/home/adam/workspace/ucl/spike_clustering/combinato
 export PATH PYTHONPATH
 
-# loop over .mat files and extract spikes
-for mat_file in *.mat; do
-	css-extract --matfile $mat_file;
-done
+for ppt_dir in */; do
 
-for directory in */; do
-    cd $directory
-    h5_file=(*h5)
-    
-    # cluster negative and positive spikes
-    css-simple-clustering --neg --datafile $h5_file
-    css-simple-clustering --datafile $h5_file
+    cd $ppt_dir
 
-    # create summary plots for negative and positive spikes
-    css-plot-sorted --neg --datafile $h5_file --label sort_neg_simple
-    css-plot-sorted --datafile $h5_file --label sort_pos_simple
+    # loop over .mat files and extract spikes
+    for mat_file in *.mat; do
+        css-extract --matfile $mat_file;
+    done
+
+    for directory in */; do
+        cd $directory
+        h5_file=(*h5)
+        
+        # cluster negative and positive spikes
+        css-simple-clustering --neg --datafile $h5_file
+        css-simple-clustering --datafile $h5_file
+
+        # create summary plots for negative and positive spikes
+        css-plot-sorted --neg --datafile $h5_file --label sort_neg_simple
+        css-plot-sorted --datafile $h5_file --label sort_pos_simple
+
+        cd ..
+    done
 
     cd ..
 done
-
 
 
